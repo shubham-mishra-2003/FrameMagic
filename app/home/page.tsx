42a51abe647a13a9977ddd66680090166c6550fe
Mayank Kumar Grade <mayankkumargrade@gmail.com>
2024-07-12 19:12:56 +0530

    Home

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 
 const page = () => {
   return (
-<<<<<<< HEAD
     <div>
       <header className="text-white" style={{
         backgroundImage: "linear-gradient(to right, rgba(79, 70, 229, 0.8), rgba(67, 56, 202, 0.8))",
@@ -93,10 +92,6 @@ const page = () => {
           </nav>
         </div>
       </footer>
-=======
-    <div className='flex justify-center items-center'>
-      Home
->>>>>>> d905a26795b42968be5505e901cd09bb879f80f7
     </div>
   )
 }
