0189e4007ce203e74d33c96a6c815780f7d3f0f5
shubham-mishra-2003 <shubhammishra3070@gmail.com>
2024-10-06 13:09:31 +0530

    main

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { SignedIn, SignedOut } from "@clerk/nextjs";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import Dashboard from "@/components/Dashboard";
 import LandingPage from "@/components/homepage/LandingPage";
 import SplashScreenWrapper from "@/components/SplashScreenWrapper";
@@ -10,7 +10,11 @@ const Page = () => {
         <LandingPage />
       </SignedOut>
       <SignedIn>
-        <Dashboard />
+        {/* <Dashboard /> */}
+        <div className="flex gap-10 p-10">
+        <h1>Logged in</h1>
+        <UserButton />
+        </div>
       </SignedIn>
     </SplashScreenWrapper>
   );
